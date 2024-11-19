5363bb8d36e90cd81f31e1d70eec282d41d3b2d2
Levi <hi@itslevir.com>
2024-11-19 16:15:06 +0000

    home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+	import '$lib/hoefler-text.ttf';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="h-screen w-screen bg-black text-white">
+	{@render children()}
+</div>
+
+<style>
+	@font-face {
+		font-family: 'Hoefler Text';
+		src: url('/hoefler-text.ttf') format('truetype');
+	}
+
+	div {
+		font-family: 'Hoefler Text', serif;
+	}
+</style>

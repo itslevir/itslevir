0e7dceee0d03b5a8f09f7b9a8f05f6f6b2e77080
Levi <hi@itslevir.com>
2024-12-20 01:35:40 +0000

    maybe

---
+++ src/routes/photography/collection/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div>
+	<main class="h-full w-full p-12">
+		<div class="flex flex-col w-full justify-center">
+			<h1 class="text-8xl">open a collection</h1>
+            <input type="text" placeholder="id" class="w-1/2 p-2 m-2"/>
+		</div>
+	</main>
+</div>

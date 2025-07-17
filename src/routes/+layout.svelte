357c480b767e97ba39967cd5f358afddc5632424
Levi <hi@itslevir.com>
2025-07-17 17:21:19 +0000

    💄 Basic styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<div class="layout">
+	<slot />
+</div>
+
+<style>
+	@font-face {
+		font-family: 'Plus Jakarta Sans Variable';
+		src: url('/plusjakartasans.ttf') format('truetype');
+		font-weight: 100 900;
+		font-style: normal;
+	}
+
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		width: 100%;
+		overflow: hidden;
+	}
+
+	.layout {
+		font-family: 'Plus Jakarta Sans Variable', sans-serif;
+		font-weight: 400;
+		color: rgb(255, 255, 255);
+		background-color: rgb(0, 15, 63);
+	}
+</style>

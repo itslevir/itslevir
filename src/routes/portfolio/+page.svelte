940d1a201c862b97f0873a0cb0038edcf2fadea2
Levi <hi@itslevir.com>
2025-07-21 20:19:15 +0000

    ✨ Photo page

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,65 @@
+<div class="content">
+	<main>
+		<h1>photos <a href="/portfolio/photos">see all</a></h1>
+		<div class="photo-collection">
+			<a href="/portfolio/photos/landscape" class="photo-item">
+				<img
+					src="https://cdn.itslevir.com/IMG_1189.JPEG"
+					alt="A snowy mountain in the distance, covered with fog. The land in front of it is dry and dusty rolling hills"
+				/>
+				<span>landscape / outdoors</span>
+			</a>
+			<a href="/portfolio/photos/portrait" class="photo-item">
+				<img
+					src="https://cdn.itslevir.com/IMG_9715.JPEG"
+					alt="An expressionless male face against a black background. A glint of light shines off his face, and he his holding a bunch of white flowers in his hand."
+				/>
+				<span>portrait / people</span>
+			</a>
+		</div>
+	</main>
+</div>
+
+<style>
+	img {
+		width: 256px;
+		height: 256px;
+		object-fit: cover;
+		border-radius: 8px;
+	}
+
+	h1 > a {
+		font-size: 1.2rem;
+		color: inherit;
+		text-decoration: none;
+		margin-left: 8px;
+	}
+
+	.content {
+		width: 100vw;
+		height: 100vh;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.photo-item {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-decoration: none;
+		color: inherit;
+		transition: transform 0.2s ease-in-out;
+	}
+
+	.photo-item span {
+		margin-top: 8px;
+		font-size: 1.2rem;
+	}
+
+	.photo-item:hover {
+		transform: scale(1.05);
+	}
+</style>

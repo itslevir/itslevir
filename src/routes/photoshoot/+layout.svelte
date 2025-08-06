95849e3087e5c10abccc880d4a2b782a299e7509
Levi <hi@itslevir.com>
2025-08-06 23:43:39 +0000

    handle photoshoots

---
+++ src/routes/photoshoot/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+
+	let { children }: LayoutProps = $props();
+</script>
+
+<div class="content">{@render children()}</div>
+
+<style>
+	.content {
+		width: 100vw;
+		height: 100vh;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
